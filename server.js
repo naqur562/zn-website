b5964a4bacb011aae823f9159fcf8f0e5dcf6255
Nabeel Qureshi <nabeel@Nabeels-MacBook-Pro.local>
2022-11-27 17:26:29 -0600

    try again

--- server.js
+++ server.js
@@ -222,12 +222,7 @@ app.post("/addevent", function(req, res){
 })
 
 
-//Launch Server
-let port = process.env.PORT
-if (port == null || port == ""){
-    port = 3000
-}
 
-app.listen(port , function(){
+app.listen(process.env.PORT || 3000 , function(){
     console.log("Server started successfully")
 })
