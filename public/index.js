6010c9f3738d928fa91629167a9327b9a0353bb5
Nabeel Qureshi <nabeel@Nabeels-MacBook-Pro.local>
2022-11-28 15:10:23 -0600

    added success and fail pages after form submission

---
+++ public/index.js
@@ -0,0 +1,6 @@
+function showModal(){
+    document.querySelector(".signup-modal").style.display = "block"
+    document.querySelector(".signup-modal").innerHTML = "<p>Form successfully submitted!</p>"
+}
+
+export default showModal
